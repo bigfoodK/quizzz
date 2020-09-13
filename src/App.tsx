30168d6b3a159bcd7715552968c34bd7983b9498
bigfoodk <satcom1600@hanmail.net>
2020-09-13 13:47:00 +0900

    Use parcel

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Container, Typography } from '@material-ui/core';
+
+export default function App() {
+  return (
+    <Container>
+      <Typography>
+        Hello world
+      </Typography>
+    </Container>
+  );
+}
